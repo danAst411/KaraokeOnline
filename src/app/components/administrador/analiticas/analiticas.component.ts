70b4532b3b32e757034fe597643e57cec152a650
Victor Astudillo <50681024+danAst411@users.noreply.github.com>
2024-11-14 07:26:26 +0000

    actualizacion

---
+++ src/app/components/administrador/analiticas/analiticas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analiticas',
+  standalone: true,
+  imports: [],
+  templateUrl: './analiticas.component.html',
+  styleUrl: './analiticas.component.css'
+})
+export class AnaliticasComponent {
+
+}
---
+++ src/app/components/administrador/sistema-calificaciones/sistema-calificaciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sistema-calificaciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './sistema-calificaciones.component.html',
+  styleUrl: './sistema-calificaciones.component.css'
+})
+export class SistemaCalificacionesComponent {
+
+}
